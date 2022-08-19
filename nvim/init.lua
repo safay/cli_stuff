1346c0cdb91a7d5a2a46b073288a0a391d9ffa59
Scott Fay <scott.a.fay@gmail.com>
2022-08-19 15:48:31 -0700

    update for nvim

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+vim.opt.syntax = 'enable'
+vim.opt.termguicolors = true
+vim.cmd('colorscheme challenger_deep')
+vim.opt.background = "dark"
+vim.opt.number = true -- " show line numbers
+vim.opt.ruler = true --
+vim.opt.tabstop = 4 -- 4 whitespaces for tabs visual presentation
+vim.opt.shiftwidth = 4 -- shift lines by 4 spaces
+vim.opt.smarttab = true -- set tabs for a shifttabs logic
+vim.opt.expandtab = true -- expand tabs into spaces
+vim.opt.autoindent = true -- indent when moving to the next line while writing code
+vim.opt.showmatch = true -- shows matching part of bracket pairs (), [], {}
+vim.opt.enc = "utf-8" -- utf-8 by default
+
